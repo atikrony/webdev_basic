7b83e1099723d74e02f16638766b1e2b4a1af2df
atikrony <atik.rony2013@gmail.com>
2025-07-04 01:14:46 +0600

    page

--- control.js
+++ control.js
@@ -3,3 +3,8 @@ function diceRandom() {
   var diceImg = document.querySelector(".container img");
   diceImg.src = "assets/dice-" + x + ".png";
 }
+
+document.getElementById("roll-btn").addEventListener("click", function () {
+  const diceNumber = Math.floor(Math.random() * 6) + 1;
+  document.getElementById("dice-img").src = `assets/dice-${diceNumber}.png`;
+});
